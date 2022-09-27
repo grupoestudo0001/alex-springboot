64333cac74edaf7525e946bae168a9f365e55c51
aresende2015 <alex.q.resende@gmail.com>
2022-09-26 21:35:47 -0300

    Refatorando o controller de Setor

--- src/alex-springboot/src/main/java/br/com/financeiro/controllers/SetorController.java
+++ src/alex-springboot/src/main/java/br/com/financeiro/controllers/SetorController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,37 +24,33 @@ public class SetorController {
 
 	@Autowired
 	private SetorService service;
-	
-	@RequestMapping(value = "/{id}",
-					method = RequestMethod.GET,
-					produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Setor BuscarPorId(@PathVariable(value = "id") Long id) {
 		return service.BuscarPorId(id);
 	}
-	
-	@RequestMapping(method = RequestMethod.GET,
-					produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Setor> BuscarTodos() {
 		return service.BuscarTodos();
 	}
-	
-	@RequestMapping(method = RequestMethod.POST,
-					consumes = MediaType.APPLICATION_JSON_VALUE,
-					produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Setor Incluir(@RequestBody Setor setor) {
 		return service.Incluir(setor);
 	}
-	
-	@RequestMapping(method = RequestMethod.PUT,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Setor Atualizar(@RequestBody Setor setor) {
 		return service.Atualizar(setor);
 	}
-	
-	@RequestMapping(value = "/{id}",
-					method = RequestMethod.DELETE)
-	public void Deletar(@PathVariable(value = "id") Long id) {
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> Deletar(@PathVariable(value = "id") Long id) {
 		service.Deletar(id);
+		return ResponseEntity.noContent().build();
 	}
 }
